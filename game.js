5c193000a56785dd8b9d19285e3540dfbb8aa3e0
dereksmtux <dereksmith2112@verizon.net>
2017-01-17 21:03:24 -0600

    Add files via upload

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 var userChoice = " ";
 var computerChoice = 0;
 var winner = " ";
+var winCounter = 0;
+var LooseCounter = 0;
+var CounterPhraseWin = "";
+var CounterPhraseLoose = "";
 
 //function for the your move button
 function yourMove (x){
@@ -34,32 +38,49 @@ function compare (choice1,choice2) {
         if (choice2 === "scissors"){
             winner = "computer chose scissors you win."; 
             document.getElementById('prize').src= "win.png"; 
+            winCounter ++;
+            CounterPhraseWin = "Games won: " + winCounter;
+
         }
         else {
          winner = "computer chose paper you lose.";
-         document.getElementById('prize').src= "lose.png"   
+         document.getElementById('prize').src= "lose.png" 
+         LooseCounter ++;
+         CounterPhraseLoose = "Games Lost: " + LooseCounter;  
         }
     }
     else if (choice1 === "paper"){
         if (choice2 === "rock") {
             winner = "computer chose rock you win.";
             document.getElementById('prize').src= "win.png";
+            winCounter ++;
+            CounterPhraseWin = "Games won: " + winCounter;
         }
         else {
             winner = "computer chose scissors you lose."; 
-            document.getElementById('prize').src= "lose.png"   
+            document.getElementById('prize').src= "lose.png" 
+            LooseCounter ++;
+            CounterPhraseLoose = "Games Lost: " + LooseCounter;   
         }
     }
     else if (choice1 === "scissors"){
         if (choice2 === "rock") {
             winner = "computer chose rock you lose"; 
-            document.getElementById('prize').src= "lose.png"   
+            document.getElementById('prize').src= "lose.png"  
+            LooseCounter ++;
+            CounterPhraseLoose = "Games Lost: " + LooseCounter;  
         }
         else {
             winner = "computer chose paper you win."; 
             document.getElementById('prize').src= "win.png";  
+            winCounter ++;
+            CounterPhraseWin = "Games won: " + winCounter;
         }
     }
     document.getElementById("prize").style.display = "inline";
     document.getElementById("result").innerHTML = winner;
+    //todo
+    document.getElementById("counterWin").innerHTML = CounterPhraseWin;
+    document.getElementById("counterLoose").innerHTML = CounterPhraseLoose;
 };
+
